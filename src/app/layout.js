36bd069b09d63073fed5379b56e95a4dd504381e
hoanglam1107 <hoanglam.nguyenthi@mcvnetworks.com>
2025-07-31 23:11:03 +0700

    Update meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,12 +10,29 @@ const MonaSans = Mona_Sans({
 
 export const metadata = {
   title: "Hoàng Lam",
-  description: "Hoàng Lam's portfolio",
+  description: "Welcome to Hoàng Lam's personal portfolio website",
+  keywords: "portfolio, Hoàng Lam, web developer, nextjs, frontend, fullstack, backend, nodejs",
+  openGraph: {
+    title: "Hoàng Lam - Portfolio",
+    url: "https://hoanglam.portfolio.showtech.cloud//",
+    image: "https://hoanglam.portfolio.showtech.cloud/og-image.png",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={MonaSans.variable}>
+            <head>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:image" content={metadata.openGraph.image} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <title>{metadata.title}</title>
+      </head>
       <body>
         {children}
       </body>
